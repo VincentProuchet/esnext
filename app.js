8c4cef277b5bb03dace3771032bc89a12a6837b1
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-08-19 13:55:33 +0200

    classe terminée
    en attente des promises

--- app.js
+++ app.js
@@ -36,12 +36,46 @@ class Trip {
     id;
     name;
     imageURL;
+    _price;
     constructor(id, name, imageURL) {
         this.id = id;
         this.name = name;
         this.imageURL = imageURL;
     }
+    tostring() {
+        return `Trip {${this.id}, ${this.name}, ${this.imageURL},${this._price} }`
+    }
+    set price(price) {
+        this._price = price;
+    }
+    get price() {
+        return this._price;
+    }
+
+    static getDefaultTrip() {
+        return new Trip('rio-de-janeiro', 'Rio-de_janeiro', 'img/rio-de-janeiro.jpg');
+    }
+
 }
 parisTrip = new Trip('paris', 'Paris', 'img/paris.jpg');
 console.log(parisTrip);
 console.log(parisTrip.name);
+console.log(parisTrip.tostring());
+parisTrip.price = 100;
+console.log(parisTrip.tostring());
+const defaultTrip = Trip.getDefaultTrip();
+console.log(defaultTrip.tostring());
+
+
+class FreeTrip extends Trip {
+    constructor(id, name, imageURL, price = 0) {
+        super(id, name, imageURL);
+        super._price = 0;
+    }
+    tostring() {
+        return `Free${super.tostring()}`;
+    }
+}
+
+const freeTrip = new FreeTrip('nates', 'Nantes', 'img/nantes.jpg')
+console.log(freeTrip.tostring());
