f3b9373c9d513ab01207722ce51c54d79b4390ff
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-08-19 12:46:24 +0200

    Classe
    pause déjeuner

--- app.js
+++ app.js
@@ -11,3 +11,37 @@ console.log(citiesId);
 citiesId.push('tokyo');
 console.log(citiesId);
 
+function getWeather(cityId = "") {
+    return {
+        city: cityId.toLocaleUpperCase(),
+        temperature: 20
+    };
+}
+
+const weather = getWeather(favoriteCityId);
+
+console.log(weather);
+let { temperature, city } = weather;
+console.log(temperature);
+console.log(city);
+
+const [parisId, nycId, ...othersCitiesId] = citiesId;
+
+console.log(parisId);
+console.log(nycId);
+console.log(othersCitiesId.length);
+
+
+class Trip {
+    id;
+    name;
+    imageURL;
+    constructor(id, name, imageURL) {
+        this.id = id;
+        this.name = name;
+        this.imageURL = imageURL;
+    }
+}
+parisTrip = new Trip('paris', 'Paris', 'img/paris.jpg');
+console.log(parisTrip);
+console.log(parisTrip.name);
