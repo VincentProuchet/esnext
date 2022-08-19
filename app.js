4d199be2ae435c8d41f74988c4135565fa29db83
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-08-19 12:27:07 +0200

    let & const

---
+++ app.js
@@ -0,0 +1,13 @@
+let favoriteCityId = 'rome';
+
+console.log(favoriteCityId);
+
+favoriteCityId = 'paris';
+console.log(favoriteCityId);
+const citiesId = ['paris', 'nyc', 'rome', 'rio-de-janeiro'];
+console.log(citiesId);
+
+// citiesId = [];
+citiesId.push('tokyo');
+console.log(citiesId);
+
